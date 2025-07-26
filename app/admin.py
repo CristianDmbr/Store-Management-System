7e278dbe5a83854565899e7c1f093cab1cea35c1
Cristian Dumbravanu <Maincristian2233@gmail.com>
2025-07-26 10:16:46 +0100

    Migrated new models

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from app.models import Restaurants, RestaurantFinance, Review, Menu, Inventory, Costumer
+from app.models import Restaurants, RestaurantFinance, Review, Menu, Inventory, CostumerOrder, Staff, ShiftManager
 
 admin.site.register(Restaurants)
