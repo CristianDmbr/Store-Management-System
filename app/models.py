3f0d59304c42f57b02ece9eb8f5d77d14d6b47b0
Cristian Dumbravanu <Maincristian2233@gmail.com>
2025-12-08 23:55:50 +0000

    Model

--- app/models.py
+++ app/models.py
@@ -3,3 +3,10 @@ from django.utils import timezone
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.core.validators import MinValueValidator, MaxValueValidator
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default = False)
+
+    def __str__(self) -> str:
+        return self.title
