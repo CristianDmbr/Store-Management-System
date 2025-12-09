62c9d6dbbeab772f74f07cd6bf7a7ecc8e4952d9
Cristian Dumbravanu <Maincristian2233@gmail.com>
2025-12-09 22:51:33 +0000

    q

--- app/views.py
+++ app/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
 
-def home(request):
-    context = {"submitted": False}  # initially nothing submitted
-
+def info(request):
+    context = {"submitted" : False}
     if request.method == "POST":
-        context["username"] = request.POST.get("username")
-        context["real_name"] = request.POST.get("real_name")
-        context["email_adress"] = request.POST.get("email_adress")
-        context["number"] = request.POST.get("number")
-        context["time"] = request.POST.get("time")
+        context["name"] = request.POST.get("name")
+        context["surname"] = request.POST.get("surname")
         context["submitted"] = True
-
+    
     return render(request, "greetings/home.html", context)
