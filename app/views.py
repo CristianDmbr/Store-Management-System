40e47b51e4ca21b9928c33a4eaea7686ed470bbd
Cristian Dumbravanu <Maincristian2233@gmail.com>
2025-12-10 09:22:17 +0000

    To Do

--- app/views.py
+++ app/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Task
 
-def info(request):
-    context = {"submitted" : False}
+def task_list(request):
     if request.method == "POST":
-        context["name"] = request.POST.get("name")
-        context["surname"] = request.POST.get("surname")
-        context["submitted"] = True
+        title = request.POST.get("title")
+        if title:
+            Task.objects.create(title = title) 
+        return redirect("task_list")
     
-    return render(request, "greetings/home.html", context)
+    tasks = Task.objects.all().order_by("-id")
+    return render(request, "tasks/task_list.html",{"tasks" : tasks})
+
+def complete_task(request, task_id):
+    task = Task.objects.get(id = task_id)
+    task.completed = True
+    task.save()
+    return redirect("task_list")
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id = task_id)
+    task.delete()
+    return redirect("task_list")
--- store/urls.py
+++ store/urls.py
@@ -20,5 +20,7 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.info, name = "introduction" )
+    path("", views.task_list, name="task_list"),
+    path("complete/<int:task_id>/", views.complete_task, name="complete_task"),
+    path("delete/<int:task_id>/", views.delete_task, name="delete_task"),
 ]
