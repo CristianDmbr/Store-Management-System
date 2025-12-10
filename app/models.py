749ab3b9ecc4291fd896042519224d31a24383e7
Cristian Dumbravanu <Maincristian2233@gmail.com>
2025-12-10 12:57:56 +0000

    status

--- app/models.py
+++ app/models.py
@@ -5,8 +5,18 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Task(models.Model):
+    STATUS_CHOICES = [
+        ('not_started', 'Not Started'),
+        ('in_progress', 'In Progress'),
+        ('almost_done', 'Almost Done')
+    ]
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default = False)
+    status = models.CharField(
+        max_length = 20,
+        choices = STATUS_CHOICES,
+        default = 'not_started'
+    )
 
     def __str__(self):
         return self.title
--- app/views.py
+++ app/views.py
@@ -4,8 +4,9 @@ from .models import Task
 def task_list(request):
     if request.method == "POST":
         title = request.POST.get("title")
-        if title:
-            Task.objects.create(title = title) 
+        status = request.POST.get("status")
+        if title and status :
+            Task.objects.create(title = title, status = status) 
         return redirect("task_list")
     
     tasks = Task.objects.all().order_by("-id")
