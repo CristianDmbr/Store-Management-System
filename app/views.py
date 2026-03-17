7e9a590a2ad10b803c1a79bcfbc62f1b2fa55fb5
Cristian Dumbravanu <Maincristian2233@gmail.com>
2026-03-17 12:00:07 +0000

    New Comments

--- app/views.py
+++ app/views.py
@@ -5,23 +5,41 @@ from django.views.generic import ListView,CreateView, UpdateView, DeleteView
 from django.views.generic.edit import FormMixin
 from django.urls import reverse_lazy
 
+# Understanding GET and POST
+# GET : "give me data" and POST : "send/change data"
+# GET and POST are part of the HTTP (HyperText Transfer Protocol / how the browser talks to the servers) 
+# e.g. When you first open the <restaurant/<int:pk>/edit"> page its a GET request for it to display the template,form and row data.
+#      When you click submit then the browser sends a POST request e.g. POST /restaurant/edit]
+# def get(self,request, *args, **kwargs): (Shows page)
+# def post(self,request), *args, **kwargs): (Handle form submission)
+# POST are usually once you submit an, edit, add, delete.
+# Why not have Delete or Edit on GET? : Imagive you load a page and it deletes your data or user clicks a link and edits a row.
+# Why not display with POST? : Page may break or not load properly.
+# SIMPLE RULE:
+# 1.GET must be SAFE(no changes on the database)
+# 2.POST is for changes
+# Inside of the templates you have <form method = "POST"> which indicates when submited browser will send a POST request to the server.
+
 def home(request):
     return render(request, "home.html",{})
 
 class RestaurantList(ListView):
     model = Restaurant
     template_name = "restaurant_list.html"
+    # Context object name allows to change the variable name which we use inside of the restaurant_list.html 
+    # e.g. : <for restaurant in restaurants>
     context_object_name = "restaurants"
 
-    # get_context_data is a prebuilt Django function which builds the context dictionary sent to the template.
+    # get_context_data is a prebuilt Django function which builds the context dictionary sent into the template.
     # Here we just added more info about the page.
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["page_title"] = "List of all restaurant sorted by date openened."
         return context
 
-    # get_queryset is a prebuilt Django function which determines what data should be retrieved from the database or
-    # how to retreive that data.
+    # get_queryset is a prebuilt Django function which determines what data should be retrieved from the database or how to retreive that data.
+    # It returns a list of data (Many objects), its used in ListView because it deals with many multiple objects.
+    # Main attributes : returns many objects, used in ListView, uses the whole database.
     def get_queryset(self):
         return Restaurant.objects.order_by("-date_opened")
 
@@ -33,17 +51,33 @@ class RestaurantCreate(CreateView):
     template_name = "restaurant_add.html"
     # reverse() converts a URL name into an actual URL. 
     # regular reverse() resolves the url name immediatelly when file loads, at that moment the url might not exist so Django will throw error.
-    # reverse_lazy doesnt resolve it immediatelly but instead it waits until URL is needed.
+    # reverse_lazy doesnt resolve it immediatelly but instead it waits until  URL is needed.
     success_url = reverse_lazy("restaurant_list")
 
 
     # UpdateView uses a get_object to get the pk and filter the exact row and then it passes it through the form.
+    # GET -> show form (with prefilled data from row), POST -> UPDATE object
 class RestaurantUpdate(UpdateView):
     model = Restaurant
     form_class = RestaurantForm
     template_name = "restaurant_update.html"
     success_url = reverse_lazy("restaurant_list")
 
+    #self.kwargs : data coming FROM the URL into the view. Dictionary of values extracted from the URL e.g. with a pk = 5 self.kwarfs = {"pk" : 5}.
+    # The reason its called "pk" in the self.kwargs is because we named it like that inside of the <int:pk>.
+    # self.kwargs (INPUT to the view) and context (OUTPUT from the view).
+
+    # get_object() returns ONE specific object from the database.
+    # Used inside views which deal with a single object: UpdateView, DeleteView, DetailView.
+    #(The pk we use inside of the kwargs is from the path converter)
+    # Main attributes : returns one object, used in UpdateView, DeleteView, DetailView, it uses pk from the path converter passed to URL.
+    # Below you will see how it works by default:
+    def get_object(self):
+        return Restaurant.objects.get(pk = self.kwargs["pk"])
+
+
+    # Delete View works almost the same as UpdateView but it retrieves ONE object using a pk and then it deletes it instead of updating it.
+    # GET -> show confirmation page (doesnt show prefilled form with data ), POST -> DELETE object
 class RestaurantDelete(DeleteView):
     model = Restaurant
     template_name = "restaurant_delete.html"
--- store/urls.py
+++ store/urls.py
@@ -18,7 +18,23 @@ from django.contrib import admin
 from django.urls import path
 from app import views
 
-# The URL patterns with passed keyworkds doesn't matter.
+# What is a URL (Uniform Resource Locator): the address of a page on a website.
+
+# Why a CBV is in form < views.RestaurantList.as_view() > and why FBV as <views.combine_form_view>?
+# FBV : its already a function and Django can call it directly without the <as_view()>
+# CBV : is a class and Django cannot call a class directly so it needs the <as_view()>.
+#   In urls.py Django expects a callable (a function it can run).
+#   So <as_view()> converts a class into a function Django can call.
+
+# The URL patterns which passed keywords doesn't matter.
+# The profesional rule for naming a path converter : <Name of template>/<Path Converter>/<Action>
+# How path converter works with the example of Restaurant:
+# 1. Inside of the restaurant_list.html we display all of the restaurants and next to each row a href ("Where the link should go")
+#   where we pass the each individual restaurant pk incase user clicks the edit or delete button.
+#   <a href="{% url 'restaurant_edit' restaurant.pk %}">Edit</a>
+# 2. When the href is clicked, we mathc the URL name to the actual URL and we pass the restaurant.pk to the view so now 
+#   self.object = Restaurant.objects.get(pk = restaurant.pk) so get_object only fetches one row and the form_class is used to display pre filled and 
+#   validate that one row.
 
 urlpatterns = [
     path('admin/', admin.site.urls),
