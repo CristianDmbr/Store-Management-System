4a1cb0ad7e5526cc775e88c8c5bf491c8d9ee07f
Cristian Dumbravanu <Maincristian2233@gmail.com>
2026-09-04 12:49:24 +0100

    Started React/JavaScript

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+      <h1>Hello from my restaurant system!</h1>
+  );
+}
+
+export default App;
